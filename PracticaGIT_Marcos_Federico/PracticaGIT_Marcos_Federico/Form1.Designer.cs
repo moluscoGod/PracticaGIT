8215606aa04a793adb19037ff9d047cf75ab80e9
Fede_LR <fedelarosa2005@gmail.com>
2024-12-10 17:09:09 +0100

    punto 11

--- PracticaGIT_Marcos_Federico/PracticaGIT_Marcos_Federico/Form1.Designer.cs
+++ PracticaGIT_Marcos_Federico/PracticaGIT_Marcos_Federico/Form1.Designer.cs
@@ -28,33 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.botton1 = new System.Windows.Forms.Button();
+            this.chkUrgente = new System.Windows.Forms.CheckBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtTelegrama = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // botton1
             // 
-            this.button1.Location = new System.Drawing.Point(559, 116);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.botton1.Location = new System.Drawing.Point(501, 330);
+            this.botton1.Name = "botton1";
+            this.botton1.Size = new System.Drawing.Size(112, 54);
+            this.botton1.TabIndex = 0;
+            this.botton1.Text = "Calcular";
+            this.botton1.UseVisualStyleBackColor = true;
+            // 
+            // chkUrgente
+            // 
+            this.chkUrgente.AutoSize = true;
+            this.chkUrgente.Location = new System.Drawing.Point(146, 311);
+            this.chkUrgente.Name = "chkUrgente";
+            this.chkUrgente.Size = new System.Drawing.Size(70, 17);
+            this.chkUrgente.TabIndex = 1;
+            this.chkUrgente.Text = "Urgente?";
+            this.chkUrgente.UseVisualStyleBackColor = true;
+            this.chkUrgente.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(162, 364);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 2;
+            this.txtPrecio.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtTelegrama
+            // 
+            this.txtTelegrama.Location = new System.Drawing.Point(146, 147);
+            this.txtTelegrama.Multiline = true;
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(467, 113);
+            this.txtTelegrama.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(143, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Texto";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Coste:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTelegrama);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.chkUrgente);
+            this.Controls.Add(this.botton1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button botton1;
+        private System.Windows.Forms.CheckBox chkUrgente;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtTelegrama;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- PracticaGIT_Marcos_Federico/PracticaGIT_Marcos_Federico/Form1.cs
+++ PracticaGIT_Marcos_Federico/PracticaGIT_Marcos_Federico/Form1.cs
@@ -16,5 +16,73 @@ namespace PracticaGIT_Marcos_Federico
         {
             InitializeComponent();
         }
+        private void btnCalcularPrecio_Click(object sender, EventArgs e)
+        {
+            string textoTelegrama;
+            char tipoTelegrama = ' ';
+            int numPalabras = 0;
+            double coste;
+            //Leo el telegrama
+            textoTelegrama = txtTelegrama.Text;
+            // telegrama urgente?
+            if (chkUrgente.Checked)
+            {
+                tipoTelegrama = 'u';
+            }
+            //Obtengo el número de palabras que forma el telegrama
+            numPalabras = textoTelegrama.Length;
+            //Si el telegrama es ordinario
+            if (tipoTelegrama == 'o')
+            {
+                if (numPalabras <= 10)
+                {
+                    coste = 2.5;
+                }
+                else
+                {
+                    coste = 0.5 * numPalabras;
+                }
+            }
+            else
+            //Si el telegrama es urgente
+            {
+                if (tipoTelegrama == 'u')
+                {
+                    if (numPalabras <= 10)
+                    {
+                        coste = 5;
+                    }
+                    else
+                    {
+                        coste = 5 + 0.75 * (numPalabras - 10);
+                    }
+                }
+                else
+                {
+                    coste = 0;
+                }
+            }
+            txtPrecio.Text = coste.ToString() + " euros";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
